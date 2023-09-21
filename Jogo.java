39e2fd8f0da2965eec1c36eb3282c98e011a94cf
Vinicius Giacomelli <Vinicius.giacomelli@fatec.sp.gov.br>
2023-09-21 17:33:18 -0300

    ap07(poo-p2): cacador implementado

--- Jogo.java
+++ Jogo.java
@@ -1,11 +1,23 @@
+import java.util.Random;
 public class Jogo {
     public static void main(String[] args) throws Exception {
+        var gerador = new Random();
         var personagem = new Personagem();
         personagem.nome = "Kleiton";
+
         while(true){
-            personagem.cacar();
-            personagem.comer();
-            personagem.dormir();
+            int oQueFazer = gerador.nextInt(3); //[0...3[
+            switch(oQueFazer){
+                case 0:
+                    personagem.cacar();
+                    break;
+                case 1:
+                    personagem.comer();
+                    break;
+                case 2:
+                    personagem.dormir();
+                    break;
+            }
             System.out.printf(
             "e: %d, f: %d, s: %d \n", 
             personagem.energia,
