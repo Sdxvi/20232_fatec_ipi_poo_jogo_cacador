35e2ab66698e278ed7232433a77d98a49c257fd7
Vinicius Giacomelli <Vinicius.giacomelli@fatec.sp.gov.br>
2023-09-21 16:32:50 -0300

    ap07(poo-p2): jogo v1

---
+++ Jogo.java
@@ -0,0 +1,13 @@
+public class Jogo {
+    public static void main(String[] args) throws Exception {
+        var personagem = new Personagem();
+        personagem.nome = "Kleiton";
+        while(true){
+            personagem.cacar();
+            personagem.comer();
+            personagem.dormir();
+            Thread.sleep(5000);
+            System.out.println("******************");
+        }
+    }
+}
