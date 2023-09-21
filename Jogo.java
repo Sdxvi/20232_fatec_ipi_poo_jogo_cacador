ffa8e9bfe1179dde473af27b09bf55b6f7be8c7a
Vinicius Giacomelli <Vinicius.giacomelli@fatec.sp.gov.br>
2023-09-21 17:42:32 -0300

    ap07(poo-p2): soneca no jogo

--- Jogo.java
+++ Jogo.java
@@ -3,6 +3,11 @@ public class Jogo {
     public static void main(String[] args) throws Exception {
         var gerador = new Random();
         var personagem = new Personagem();
+        var soneca = new Personagem();
+        soneca.nome = "Itallo";
+        soneca.sono = 10;
+        soneca.energia = 0;
+        soneca.fome = 4;
         personagem.nome = "Kleiton";
 
         while(true){
@@ -24,8 +29,21 @@ public class Jogo {
             personagem.fome,
             personagem.sono
             );
-            Thread.sleep(5000);
+            System.out.println("------------------");
+            var oQueOSonecaVaiFazer = gerador.nextDouble();
+            if (oQueOSonecaVaiFazer <= 0.8) soneca.dormir();
+            else if (oQueOSonecaVaiFazer <= 0.9) soneca.comer();
+            else soneca.cacar();
+
+            System.out.printf(
+            "e: %d, f: %d, s: %d \n", 
+            soneca.energia,
+            soneca.fome,
+            soneca.sono
+            );
+
             System.out.println("******************");
+            Thread.sleep(5000);
         }
     }
 }
