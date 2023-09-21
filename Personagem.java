088bc0ea9cdaf22fbcd51c3649f73cf77da17130
Vinicius Giacomelli <Vinicius.giacomelli@fatec.sp.gov.br>
2023-09-21 15:50:11 -0300

    ap07(poo-p2): intro personagem

---
+++ Personagem.java
@@ -0,0 +1,21 @@
+public class Personagem{
+    //atributos ou propriedades
+    String nome;
+    int energia;
+    int fome;
+    int sono;
+
+    //comportamentos (métodos)
+    void cacar(){
+        System.out.println(nome + " Cacando . . .");
+    }
+
+    void comer(){
+        System.out.println(nome + " Comendo . . .");
+    }
+
+    void dormir(){
+        System.out.println(nome + " Nanando . . .");
+    }
+
+}
---
+++ TestePersonagem.java
@@ -0,0 +1,11 @@
+public class TestePersonagem {
+    public static void main(String[] args) {
+        
+        var p = new Personagem();
+        p.nome = "Kleiton";
+        p.cacar();
+        p.comer();
+        p.dormir();
+        
+    }
+}
